5810a79629642ec44565d88300a720c17530f8f9
Przemog1 <minikers21@gmail.com>
2019-06-02 00:22:03 +0200

    aabbSIMD template specialization for core::vector2df_SIMD

--- include/irr/core/math/aabb2dSIMD.h
+++ include/irr/core/math/aabb2dSIMD.h
@@ -1,4 +1,4 @@
-#ifndef __IRR_AABB_SIMD_2D_H_INCLUDED__
+#ifdef __IRR_AABB_SIMD_2D_H_INCLUDED__
 #define __IRR_AABB_SIMD_2D_H_INCLUDED__
 
 #include "irr/core/math/irrMath.h"
@@ -11,7 +11,7 @@
 
 namespace irr { namespace core {
 
-
+	//max, addPoint
 class aabb2dSIMDf
 {
 public:
@@ -33,7 +33,7 @@ public:
 
 	inline core::vectorSIMDf getMinEdge() const
 	{
-		core::vectorSIMDf minEdge = internalBoxRepresentation;
+		core::vectorSIMDf minEdge = internalBoxRepresentation ^ SIGN_FLIP_MASK_XY;
 		minEdge.makeSafe2D();
 
 		return minEdge;
@@ -47,91 +47,7 @@ public:
 		return maxEdge;
 	}
 
-	//! Resets the bounding box to a one-point box.
-	/** \param x X coord of the point.
-	\param y Y coord of the point. */
-	inline void reset(const core::vector2df_SIMD& point)
-	{
-		internalBoxRepresentation = point.xyxy();
-		internalBoxRepresentation ^= SIGN_FLIP_MASK_XY;
-	}
-
-	//! Get center of the bounding box
-	/** \return Center of the bounding box. */
-	inline core::vectorSIMDf getCenter() const
-	{
-		return (internalBoxRepresentation.zwzw() - internalBoxRepresentation) * vectorSIMDf(0.5f, 0.5f, 0.f, 0.f);
-	}
-
-
-	//! Get extent of the box (maximal distance of two points in the box)
-	/** \return Extent of the bounding box. */
-	inline core::vectorSIMDf getExtent() const
-	{
-		core::vectorSIMDf extent = internalBoxRepresentation.zwzw() + internalBoxRepresentation;
-		extent.makeSafe2D();
-
-		return extent;
-	}
-
-	//! Check if the box is empty.
-	/** This means that there is no space between the min and max edge.
-	\return True if box is empty, else false. */
-	inline bool isEmpty() const
-	{
-		return ((internalBoxRepresentation ^ SIGN_FLIP_MASK_XY) == internalBoxRepresentation.zwzw()).all();
-	}
-
-	//! Get the surface area of the box in squared units
-	inline float getArea() const
-	{
-		core::vectorSIMDf a = internalBoxRepresentation + internalBoxRepresentation.zwxx();
-		return a.x * a.y;
-	}
-
-	//! Stores all 4 edges of the box into an array
-	/**
-	D ----- C
-	|		|
-	|		|
-	A ----- B
-
-	array modified by this function will store edges in this order:
-	A, B, C, D
-	\param edges: Pointer to array of 4 edges. */
-	void getCorners(core::vectorSIMDf* corners) const
-	{
-		core::vectorSIMDf ordRepBox = internalBoxRepresentation ^ SIGN_FLIP_MASK_XY;
-
-		
-		corners[0] = ordRepBox.xyzw();	//A
-		corners[1] = ordRepBox.zyzw();	//B
-		corners[2] = ordRepBox.zwzw();	//C
-		corners[3] = ordRepBox.xwzw();	//D
-
-		for (int i = 0; i < 4; i++)
-			corners[i].makeSafe2D();
-	}
-
-	//! Repairs the box.
-	/** Necessary if for example MinEdge and MaxEdge are swapped. */
-	inline core::aabb2dSIMDf& repair()
-	{
-		reset(getMinEdge());
-		addPoint(getMaxEdge());
-
-		return *this;
-	}
-
-	//! Determines if a point is within this box.
-	/** Border is included (IS part of the box)!
-	\param p: Point to check.
-	\return True if the point is within the box and false if not */
-	inline bool isPointInside(const core::vector2df_SIMD& p) const
-	{
-		core::vector4db_SIMD result = (p.xyxy() ^ SIGN_FLIP_MASK_XY) <= internalBoxRepresentation;
-		return result.allBits();
-	}
+	
 
 	//! Determines if a point is within this box and not its borders.
 	/** Border is excluded (NOT part of the box)!
--- include/irr/core/math/aabb2dSIMDIntBase.h
+++ include/irr/core/math/aabb2dSIMDIntBase.h
@@ -4,9 +4,12 @@
 #include "irr/core/math/irrMath.h"
 #include "irr/core/memory/memory.h"
 #include "irr/core/alloc/AlignedBase.h"
-#include "C:\IrrlichtBAW\IrrlichtBAW\include\vectorSIMD.h"
+#include "../include/vectorSIMD.h"
 
 #include <iostream>
+
+#define DWN_CAST_THIS_PTR static_cast<const CRTP<VECTOR_TYPE>*>(this)
+#define SIGN_FLIP_MASK_XY	core::vector3du32_SIMD(0x80000000, 0x80000000, 0x00000000, 0x00000000)
 
 namespace irr { namespace core {
 
@@ -14,57 +17,74 @@ namespace irr { namespace core {
 template<template<typename> typename CRTP, typename VECTOR_TYPE>
 class aabbSIMDBase
 {
-	static_assert(	
-		std::is_same<VECTOR_TYPE, core::vector4di32_SIMD>::value ||
-		std::is_same<VECTOR_TYPE, core::vector4du32_SIMD>::value, 
+	static_assert(
+		std::is_same<VECTOR_TYPE, core::vector2di32_SIMD>::value ||
+		std::is_same<VECTOR_TYPE, core::vector2du32_SIMD>::value ||
+		std::is_same<VECTOR_TYPE, core::vector2df_SIMD>::value,
 		"assertion failed: cannot use this type");
 
 public:
+
 	inline void addPoint(const VECTOR_TYPE& other)
 	{
-		static_cast<CRTP<VECTOR_TYPE>*>(this)->addBox();
+		DWN_CAST_THIS_PTR->addBox();
 	}
 
 	inline VECTOR_TYPE getExtent() const
 	{
-		return getMaxEdge() - getMinEdge();
+		return DWN_CAST_THIS_PTR->getMaxEdge() - static_cast<const CRTP<VECTOR_TYPE>*>(this)->getMinEdge();
 	}
 
-	inline VECTOR_TYPE getMinEdge() const
+	//need operator/ for core::vector32_i<uint32_t> and core::vector32_i<uint32_t>
+	/*inline VECTOR_TYPE getCenter() const
 	{
-		VECTOR_TYPE minEdge = box;
-		minEdge.makeSafe2D();
-		return minEdge;
-	}
+		return static_cast<const CRTP<VECTOR_TYPE>*>(this)->getMaxEdge() - static_cast<const CRTP<VECTOR_TYPE>*>(this)->getMinEdge();
+	}*/
 
-	inline VECTOR_TYPE getMaxEdge() const
+	//! Resets the bounding box to a one-point box.
+	/** \param x X coord of the point.
+	\param y Y coord of the point. */
+	inline void reset(const VECTOR_TYPE& point)
 	{
-		VECTOR_TYPE maxEdge = box.zwzw();
-		maxEdge.makeSafe2D();
-		return maxEdge;
-	}
-
-	inline CRTP<VECTOR_TYPE> reset(const VECTOR_TYPE& point) const
-	{
-		box = point.xyxy();
-		return *this;
+		CRTP<VECTOR_TYPE> pointBox(point.xyxy());
+		internalBoxRepresentation= pointBox;
 	}
 
 	//! Repairs the box.
 	/** Necessary if for example MinEdge and MaxEdge are swapped. */
 	inline CRTP<VECTOR_TYPE> repair()
 	{
-		reset(getMinEdge());
-		addPoint(getMaxEdge());
+		//
+		//but will it work for aabbSIMDf for sure?
+		//
+		//
+		reset(DWN_CAST_THIS_PTR->getMinEdge());
+		addPoint(DWN_CAST_THIS_PTR->getMaxEdge());
 
 		return *this;
+	}
+
+	//! Check if the box is empty.
+	/** This means that there is no space between the min and max edge.
+	\return True if box is empty, else false. */
+	inline bool isEmpty() const
+	{
+		return return (getMinEdge() == getMaxEdge()).all();;
 	}
 
 	//! Get the surface area of the box in squared units
 	inline float getArea() const
 	{
-		core::vector4di32_SIMD a = box + box.zwzw();
-		return a.x* a.y;
+		VECTOR_TYPE a = DWN_CAST_THIS_PTR->internalBoxRepresentation.getMaxEdge().x - DWN_CAST_THIS_PTR->internalBoxRepresentation.getMinEdge().x;
+		VECTOR_TYPE b = DWN_CAST_THIS_PTR->internalBoxRepresentation.getMaxEdge().y - DWN_CAST_THIS_PTR->internalBoxRepresentation.getMinEdge().y;
+		return a * b;
+	}
+
+	//! Get center of the bounding box
+	/** \return Center of the bounding box. */
+	inline core::vectorSIMDf getCenter() const
+	{
+		return (internalBoxRepresentation.zwzw() - internalBoxRepresentation) / VECTOR_TYPE(2);
 	}
 
 	//! Determines if the axis-aligned box intersects with another axis-aligned box.
@@ -74,14 +94,14 @@ public:
 	bool intersectsWithBox(const CRTP<VECTOR_TYPE>& other) const
 	{
 		return
-			static_cast<const CRTP<VECTOR_TYPE>*>(this)->isPointInside(other.box) ||
-			static_cast<const CRTP<VECTOR_TYPE>*>(this)->isPointInside(other.box.zwxx()) ||
-			static_cast<const CRTP<VECTOR_TYPE>*>(this)->isPointInside(other.box.xwxx()) ||
-			static_cast<const CRTP<VECTOR_TYPE>*>(this)->isPointInside(other.box.zyxx());
+			DWN_CAST_THIS_PTR->isPointInside(other.internalBoxRepresentation) ||
+			DWN_CAST_THIS_PTR->isPointInside(other.internalBoxRepresentation.zwxx()) ||
+			DWN_CAST_THIS_PTR->isPointInside(other.internalBoxRepresentation.xwxx()) ||
+			DWN_CAST_THIS_PTR->isPointInside(other.internalBoxRepresentation.zyxx());
 	}
 
 protected:
-	VECTOR_TYPE box;
+	VECTOR_TYPE internalBoxRepresentation;
 };
 
 template<typename VECTOR_TYPE>
@@ -90,13 +110,29 @@ class aabbSIMD : public aabbSIMDBase<aabbSIMD, VECTOR_TYPE>
 
 
 template<>
-class aabbSIMD<core::vector4di32_SIMD> : public aabbSIMDBase<aabbSIMD,core::vector4di32_SIMD>
+class aabbSIMD<core::vector2di32_SIMD> : public aabbSIMDBase<aabbSIMD,core::vector2di32_SIMD>
 {
 public:
 	aabbSIMD(const core::vector4di32_SIMD& _box)
 		//:box(_box) - doesn't compile
 	{
-		this->box = _box;
+		this->internalBoxRepresentation = _box;
+	}
+
+	inline core::vector2di32_SIMD getMinEdge() const
+	{
+		core::vector2di32_SIMD minEdge = internalBoxRepresentation;
+		minEdge.makeSafe2D();
+
+		return minEdge;
+	}
+
+	inline core::vector2di32_SIMD getMaxEdge() const
+	{
+		core::vector2di32_SIMD maxEdge = internalBoxRepresentation.zwzw();
+		maxEdge.makeSafe2D();
+
+		return maxEdge;
 	}
 
 	bool isPointInside(const core::vector2di32_SIMD& point) const
@@ -111,9 +147,83 @@ public:
 
 };
 
-typedef aabbSIMD<core::vector4di32_SIMD> aabbSIMDi;
+template<>
+class aabbSIMD<core::vector2df_SIMD> : public aabbSIMDBase<aabbSIMD, core::vector2df_SIMD>
+{
+public:
+	inline aabbSIMD()
+		//: internalBoxRepresentation(-FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX)
+	{
+		internalBoxRepresentation = core::vectorSIMDf(-FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX);
+	};
+
+	aabbSIMD(const core::vector2df_SIMD& _box)
+		//:inernalboxRepresentation(_box) - doesn't compile
+	{
+		this->internalBoxRepresentation = _box ^ SIGN_FLIP_MASK_XY;
+	}
+
+	inline void addBox(const aabbSIMD<core::vector2df_SIMD>& other)
+	{
+		internalBoxRepresentation = core::max_(internalBoxRepresentation, other.internalBoxRepresentation);
+	}
+
+	inline core::vector2df_SIMD getMinEdge() const
+	{
+		core::vector2df_SIMD minEdge = internalBoxRepresentation ^ SIGN_FLIP_MASK_XY;
+		minEdge.makeSafe2D();
+
+		return minEdge;
+	}
+
+	inline core::vector2df_SIMD getMaxEdge() const
+	{
+		core::vector2df_SIMD maxEdge = internalBoxRepresentation.zwzw();
+		maxEdge.makeSafe2D();
+
+		return maxEdge;
+	}
+
+	//! Determines if a point is within this box.
+	/** Border is included (IS part of the box)!
+	\param p: Point to check.
+	\return True if the point is within the box and false if not */
+	inline bool isPointInside(const core::vector2df_SIMD& p) const
+	{
+		core::vector4db_SIMD result = (p.xyxy() ^ SIGN_FLIP_MASK_XY) <= internalBoxRepresentation;
+		return result.allBits();
+	}
+
+	//! Determines if a point is within this box and not its borders.
+	/** Border is excluded (NOT part of the box)!
+	\param p: Point to check.
+	\return True if the point is within the box and false if not. */
+	inline bool isPointTotalInside(const core::vector2df_SIMD& p) const
+	{
+		core::vector4db_SIMD result = (p.xyxy() ^ SIGN_FLIP_MASK_XY) < internalBoxRepresentation;
+		return result.allBits();
+	}
+
+	//! Check if this box is completely inside the 'other' box.
+	/** \param other: Other box to check against.
+	\return True if this box is completly inside the other box,
+	otherwise false. */
+	inline bool isFullInside(const aabbSIMD<core::vector2df_SIMD>& other) const
+	{
+		core::vector4db_SIMD a = internalBoxRepresentation<= other.internalBoxRepresentation;
+		return a.allBits();
+	}
+
+};
+
+typedef aabbSIMD<core::vector2df_SIMD> aabb2dSIMDf;
+typedef aabbSIMD<core::vector2di32_SIMD> aabb2dSIMDi;
+typedef aabbSIMD<core::vector2du32_SIMD> aabb2dSIMDu;
 
 }
-}
+} 
+
+#undef DWN_CAST_THIS_PTR
+#undef SIGN_FLIP_MASK_XY
 
 #endif
